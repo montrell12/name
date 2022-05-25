52e981e89117578b6c8e07547ea397c050c3bd60
montrell12 <montrelldpegues@gmail.com>
2022-05-25 12:56:21 -0500

    server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+let app = express()
+const listen = process.env.PORT || 3200
+
+app.use(express.static(path.join(__dirname, '/assets')));
+app.use(express.static(path.join(__dirname, '/vendor')));
+app.set('views', path.join(__dirname, '/src/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
+
+
+// server.secure(credentials)
+app.listen(listen, function() {
+    console.log('server is up')
+})
