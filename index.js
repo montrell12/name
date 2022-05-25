c06dfb6ca8bd1b348edea82d0700a13e2a820931
montrell12 <montrelldpegues@gmail.com>
2022-05-25 13:41:37 -0500

    css stuff

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 let app = express()
 const listen = process.env.PORT || 3200
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/assets')));
 // app.use(express.static(path.join(__dirname, '/vendor')));
 app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'ejs');
